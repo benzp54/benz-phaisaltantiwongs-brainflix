74e9a48c8a1c80c2284a5ff79e181e9ee53370e1
benzp <benz.pwongs@live.com>
2022-05-16 06:08:01 -0700

    Updated suggestions component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import Video from './components/Video/Video';
-import Comments from './components/Comments/Comments';
+import CommentsNew from './components/CommentsNew/CommentsNew';
+import CommentsList from './components/CommentsList/CommentsList';
+import Suggestions from './components/Suggestions/Suggestions';
 import React from 'react';
 
-class App extends React.Component{
-  constructor(){
-    super();
-  }
-  render() {
+//Add state to update active video & video list
+  //Add a constructor to enable use of state
+
+function App() {
     return (
       <>
       <Header />
       <Video />
-      <Comments />
+      <CommentsNew />
+      <CommentsList />
+      <Suggestions />
       </>
       );
-    }
-}
+};
 
 export default App;
