41b9857168f9b30dc74ee76ca31fc048d5c60eae
benzp <benz.pwongs@live.com>
2022-05-16 19:20:14 -0700

    Updated props and states

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import Video from './components/Video/Video';
 import CommentsNew from './components/CommentsNew/CommentsNew';
 import CommentsList from './components/CommentsList/CommentsList';
 import Suggestions from './components/Suggestions/Suggestions';
+import videoData from './data/video-details.json';
 import React from 'react';
 
 //Add state to update active video & video list
   //Add a constructor to enable use of state
 
-function App() {
+class App extends React.Component{
+
+  constructor() {
+    super();
+
+    this.state = {videoActive: videoData[0]}
+  }
+
+  render() {
     return (
       <>
       <Header />
-      <Video />
+      <Video videoActive = {this.state.videoActive}/>
       <CommentsNew />
-      <CommentsList />
+      <CommentsList videoActive = {this.state.videoActive}/>
       <Suggestions />
       </>
-      );
+    );
+  }
 };
 
 export default App;
--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,27 +1,32 @@
 import './Suggestions.scss';
-import videoData from '../../data/video-details.json';
+import videos from '../../data/videos.json';
 
-function videoList () {
+function videoList (props) {
     return (
         <section className="vidList">
             <p className="vidList__heading">
                 NEXT VIDEOS
             </p>
-            <div className="vidList__item">
-                <div className="vidList__preview">
-                    <img className="vidList__img" src={videoData[1].image} alt="Video Preview"></img>
-                </div>
-                <div className="vidList__details">
-                    <p className="vidList__title">
-                        {videoData[1].title}
-                    </p>
-                    <span className="vidList__channel">
-                        {videoData[1].channel}
-                    </span>
-                </div>
-            </div>
+
+            {videos.map((vid) => { 
+                return (
+                    <div className="vidList__item" key={vid.id}>
+                        <div className="vidList__preview">
+                            <img className="vidList__img" src={vid.image} alt="Video Preview"></img>
+                        </div>
+                        <div className="vidList__details">
+                            <p className="vidList__title">
+                                {vid.title}
+                            </p>
+                            <span className="vidList__channel">
+                                {vid.channel}
+                            </span>
+                        </div>
+                    </div>
+                );
+            })}
         </section>
-    )
+    );
 };
 
 export default videoList
